5f3b9befc4abeb46b27696f0cb7f2eaa873c9252
PrayerEzio <prayer@crucis.cn>
2019-04-08 16:37:05 +0800

    清除网站favicon.ico

--- resources/views/Admin/main.blade.php
+++ resources/views/Admin/main.blade.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="@yield('seo_keywords','Crucis后台管理系统,后台管理系统')">
     <meta name="description" content="@yield('seo_description','Crucis是一个简洁优雅的后台管理系统')">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="favicon.ico">
+
     <link href="{{ asset('assets/Admin',config('crucis.http_secure')) }}/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link href="{{ asset('assets/Admin',config('crucis.http_secure')) }}/css/animate.min.css" rel="stylesheet">
